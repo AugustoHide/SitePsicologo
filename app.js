45a9a16d27a7c83792cb6592bb66bbf2a65aaf3b
AugustoHide <augustohide@gmail.com>
2021-12-28 10:40:26 -0400

    inicialização do projeto + incialização do layout inicial

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+
+app.set('view engine', 'ejs');
+const viewPath = path.join(__dirname, '/views');
+app.set('views', viewPath);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', (req, res) => {
+    res.render('paginaInicial');
+})
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}!!!`);
+});
