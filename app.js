23fa631faa07f0c5b3a6ef1387170c55ff4d58ae
AugustoHide <augustohide@gmail.com>
2022-01-11 22:10:41 -0400

    inicio de criação de páginas: depressão, prientação, sobre, etc + termino de criação página depressão

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
+const ejsMate = require('ejs-mate');
 const path = require('path');
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 const viewPath = path.join(__dirname, '/views');
 app.set('views', viewPath);
@@ -12,7 +14,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.render('paginaInicial');
-})
+});
+
+app.get('/depressao', (req, res) => {
+    res.render('depressao');
+});
+
+app.get('/orientacaoProfissional', (req, res) => {
+    res.render('orientacaoProfissional');
+});
+
+app.get('/sobreMim', (req, res) => {
+    res.render('sobreMim');
+});
+
+app.get('/contato', (req, res) => {
+    res.render('contato');
+});
 
 
 const port = process.env.PORT || 3000;
